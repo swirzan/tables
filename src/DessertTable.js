77abb38877c10303b284e13957e4497277b150f4
swirzan <swirzan@gmail.com>
2022-11-14 11:16:12 +0500

    made edit function work

--- src/DessertTable.js
+++ src/DessertTable.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { dessertTableData } from "./TableData";
 import EditTableMsg from "./EditTableMsg";
 import EditRowForm from "./EditRowForm";
+// import EditForm from "./EditForm";
 
 const DessertTable = () => {
   const [tableData, setTableData] = useState(dessertTableData);
@@ -17,46 +18,43 @@ const DessertTable = () => {
     setEditing(true);
   };
 
-  const editTableRow = () => {
-    console.log(editItemId);
-    setEditing(false);
-  };
-
   return (
-    <table>
-      <tr>
-        <th>Dessert(100g serving)</th>
-        <th>Calories</th>
-        <th>Fat(g)</th>
-        <th>Cards(g)</th>
-        <th>Protein(g)</th>
-        <th>Sodium(mg)</th>
+    <>
+      <table>
+        <tr>
+          <th>Dessert(100g serving)</th>
+          <th>Calories</th>
+          <th>Fat(g)</th>
+          <th>Cards(g)</th>
+          <th>Protein(g)</th>
+          <th>Sodium(mg)</th>
 
-        <th>Calcium(%)</th>
-        <th>Iron(%)</th>
-        <th>Action</th>
-      </tr>
-      {tableData.map((item) => {
-        return (
-          <tr>
-            <td>{item.dessert}</td>
-            <td>{item.calories}</td>
-            <td>{item.fat}</td>
-            <td>{item.carbs}</td>
-            <td>{item.protein}</td>
-            <td>{item.sodium}</td>
-            <td>{item.calcium}</td>
-            <td>{item.iron}</td>
-            <td>
-              <button
-                onClick={() => {
-                  editHandler(item.id);
-                }}
-              >
-                Edit
-              </button>
-            </td>
-            {/* <td>
+          <th>Calcium(%)</th>
+          <th>Iron(%)</th>
+          <th>Action</th>
+        </tr>
+        {tableData.map((item) => {
+          return (
+            <tr>
+              <td>{item.dessert}</td>
+              <td>{item.calories}</td>
+              <td>{item.fat}</td>
+              <td>{item.carbs}</td>
+              <td>{item.protein}</td>
+              <td>{item.sodium}</td>
+              <td>{item.calcium}</td>
+              <td>{item.iron}</td>
+              <td>
+                <button
+                  onClick={() => {
+                    editHandler(item.id);
+                    setEditing(true);
+                  }}
+                >
+                  Edit
+                </button>
+              </td>
+              {/* <td>
               <button
                 onClick={() => {
                   props.deleteHandler(item.id);
@@ -65,18 +63,22 @@ const DessertTable = () => {
                 Delete
               </button>
             </td> */}
-          </tr>
-        );
-      })}
-      {editing && <EditTableMsg editMsg={editItemId.dessert} />}
+            </tr>
+          );
+        })}
+        {editing && <EditTableMsg editMsg={editItemId.dessert} />}
+      </table>
       {editing && (
         <EditRowForm
-          editRow={editTableRow}
-          // editItem={edited}
-          // message="Your Row is Updated"
+          editItemId={editItemId}
+          setEditItemId={setEditItemId}
+          tableData={tableData}
+          setTableData={setTableData}
         />
       )}
-    </table>
+
+      {/* <EditForm /> */}
+    </>
   );
 };
 
--- src/EditRowForm.js
+++ src/EditRowForm.js
@@ -1,31 +1,86 @@
 import React from "react";
 
 const EditRowForm = (props) => {
+  const editItemId = props.editItemId;
+  const setEditItemId = props.setEditItemId;
+  const tableData = props.tableData;
+  const setTableData = props.setTableData;
+  console.log(editItemId);
+  const changeHandler = (e) => {
+    setEditItemId((pre) => ({
+      ...pre,
+      [e.target.name]: e.target.value,
+    }));
+    console.log(setEditItemId);
+  };
   return (
-    <form name="editForm">
-      {props.editItem}
-      {props.message}
+    <form>
+      {/* {props.editItem}
+      {props.message} */}
       <input
         type="text"
         placeholder="Dessert..."
         name="dessert"
-        // value={props.editRow.dessert}
+        value={editItemId.dessert}
+        onChange={changeHandler}
       />
 
-      <input type="number" placeholder="calories..." name="calories" />
+      <input
+        type="number"
+        placeholder="calories..."
+        name="calories"
+        value={editItemId.calories}
+        onChange={changeHandler}
+      />
 
-      <input type="number" placeholder="fat..." name="fat" />
+      <input
+        type="number"
+        placeholder="fat..."
+        name="fat"
+        value={editItemId.fat}
+        onChange={changeHandler}
+      />
 
-      <input type="number" placeholder="carbs..." name="carbs" />
+      <input
+        type="number"
+        placeholder="carbs..."
+        name="carbs"
+        value={editItemId.carbs}
+        onChange={changeHandler}
+      />
 
-      <input type="number" placeholder="protein..." name="protein" />
+      <input
+        type="number"
+        placeholder="protein..."
+        name="protein"
+        value={editItemId.protein}
+        onChange={changeHandler}
+      />
 
-      <input type="number" placeholder="sodium..." name="sodium" />
+      <input
+        type="number"
+        placeholder="sodium..."
+        name="sodium"
+        value={editItemId.sodium}
+        onChange={changeHandler}
+      />
 
-      <input type="text" placeholder="calcium..." name="calcium" />
+      <input
+        type="text"
+        placeholder="calcium..."
+        name="calcium"
+        value={editItemId.calcium}
+        onChange={changeHandler}
+      />
 
-      <input type="text" placeholder="iron..." name="iron" />
-      <button onClick={props.editRow}>Update</button>
+      <input
+        type="text"
+        placeholder="iron..."
+        name="iron"
+        value={editItemId.iron}
+        onChange={changeHandler}
+      />
+      <button type="submit">Update</button>
     </form>
   );
 };
